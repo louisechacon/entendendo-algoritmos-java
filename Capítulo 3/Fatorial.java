2db41af8a36c934a2f93bf184cf48495276013f2
Louise Chacon <louisechacon26@gmail.com>
2025-09-08 20:51:52 -0300

    Ajusta indentação

--- Capítulo 3/Fatorial.java
+++ Capítulo 3/Fatorial.java
@@ -3,14 +3,13 @@ public class Fatorial {
         int x = 5;
         int fatorial = calculaFatorial(x);
         System.out.printf("O fatorial de %d é %d", x, fatorial);
-        }
-
-        public static int calculaFatorial(int x) {
-            if (x == 1) {
-                return 1;
-            } else {
-                return x * calculaFatorial(x-1);
-            }
-        }
-
     }
+
+    public static int calculaFatorial(int x) {
+        if (x == 1) {
+            return 1;
+        } else {
+            return x * calculaFatorial(x-1);
+        }
+    }
+}
