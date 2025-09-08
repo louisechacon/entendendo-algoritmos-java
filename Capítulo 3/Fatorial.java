e7b85b0adb33a51581b2eb85b2f0696bb3fc894c
Louise Chacon <louisechacon26@gmail.com>
2025-09-08 20:45:23 -0300

    Adiciona função recursiva para calcular fatorial de um número

---
+++ Capítulo 3/Fatorial.java
@@ -0,0 +1,16 @@
+public class Fatorial {
+    public static void main (String[] args) {
+        int x = 5;
+        int fatorial = calculaFatorial(x);
+        System.out.printf("O fatorial de %d é %d", x, fatorial);
+        }
+
+        public static int calculaFatorial(int x) {
+            if (x == 1) {
+                return 1;
+            } else {
+                return x * calculaFatorial(x-1);
+            }
+        }
+
+    }
--- Capítulo 3/README.md
+++ Capítulo 3/README.md
@@ -1,2 +1,5 @@
 # Contagem Regressiva
 Uma função que imprime contagem regressiva utilizando recursão.
+
+# Fatorial
+Uma função recursiva para calcular o fatorial de um número.
